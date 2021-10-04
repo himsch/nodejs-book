19d31f05c91fc7b9ee91033c13528227a51bc38e
song <55rsearchco@gmail.com>
2021-10-04 15:24:44 +0900

    cookie 와 session 이해하기

---
+++ ch4/cookie/cookie2.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs').promises;
+const url = require('url');
+const qs = require('querystring');
+
+const parseCookies = (cookie = '') =>
+  cookie
+    .split(';')
+    .map(v => v.split('='))
+    .reduce((acc, [k, v]) => {
+      acc[k.trim()] = decodeURIComponent(v);
+      return acc;
+    }, {});
+
+http.createServer(async (req, res) => {
+  const cookies = parseCookies(req.headers.cookie);
+  if (req.url.startsWith('/login')) {
+    const { query } = url.parse(req.url);
+    const { name } = qs.parse(query);
+  }
+});
