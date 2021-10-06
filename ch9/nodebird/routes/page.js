c1d91c2a11aec2d4500dc5a8fee068eff363663c
song <55rsearchco@gmail.com>
2021-10-07 08:24:45 +0900

    NodeBird. multer 모듈 이해하기

--- ch9/nodebird/routes/page.js
+++ ch9/nodebird/routes/page.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.locals.user = null;
+  res.locals.user = req.user;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
-  res.locals.follower = [];
+  res.locals.followerIdList = [];
   next();
 });
 
@@ -18,13 +19,24 @@ router.get('/join', (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 });
 
-router.get('/', (req, res, next) => {
-  const twits = [];
-  res.render('main', {
-    title: 'NodeBird',
-    twits,
-    user: req.user,
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+      user: req.user,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
