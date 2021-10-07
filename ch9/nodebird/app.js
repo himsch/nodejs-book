672f427bc105c8b80cb2e01a502f01d419582f6e
song <55rsearchco@gmail.com>
2021-10-07 15:12:43 +0900

    NodeBird. 팔로잉 팔로워 구현하기

--- ch9/nodebird/app.js
+++ ch9/nodebird/app.js
@@ -11,6 +11,7 @@ dotenv.config();
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
@@ -56,6 +57,7 @@ app.use(passport.session());
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
 app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 // 404처리 미들웨어
 app.use((req, res, next) => {
